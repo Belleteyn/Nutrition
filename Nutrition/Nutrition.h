7328c3e8d0968adff6c8561bf5277e601a80b82c
Serafima Zykova <zykovas91@gmail.com>
2017-11-02 22:51:29 +0300

    Nutrition operators overloading

--- Nutrition/Nutrition.h
+++ Nutrition/Nutrition.h
@@ -5,10 +5,39 @@
 
 struct Nutrition
 {
+  float kkal = 0;
   float proteins = 0;
   float carbohydrates = 0;
   float fats = 0;
-  uint16_t kkal = 0;
+
+  Nutrition(float kkal, float p, float c, float f)
+    : kkal(kkal)
+    , proteins(p)
+    , carbohydrates(c)
+    , fats(f)
+  {}
+
+  Nutrition operator + (const Nutrition& rhs) const
+  {
+    return Nutrition(kkal + rhs.kkal, proteins + rhs.proteins, carbohydrates + rhs.carbohydrates, fats + rhs.fats);
+  }
+
+  bool operator <= (const Nutrition& rhs) const
+  {
+    return (kkal <= rhs.kkal
+        && proteins <= rhs.proteins
+        && carbohydrates <= rhs.carbohydrates
+        && fats <= rhs.fats);
+  }
+
+  Nutrition& operator += (const Nutrition& rhs)
+  {
+    kkal += rhs.kkal;
+    proteins += rhs.proteins;
+    carbohydrates += rhs.carbohydrates;
+    fats += rhs.fats;
+    return *this;
+  }
 };
 
 #endif // NUTRITION_H
