9a70ccb0ba26d8c92e2bbd90400a331a510081bc
Serafima Zykova <zykovas91@gmail.com>
2017-11-04 15:40:13 +0300

    todo for genetic #1

--- Nutrition/Genetic.cpp
+++ Nutrition/Genetic.cpp
@@ -6,5 +6,14 @@ NutritionError Algorithms::Genetic::apply(FoodContainer& foods)
 {
   std::list<FoodContainer> genotypes;
 
+  for (auto iter = foods.begin(); iter != foods.end(); ++iter)
+  {
+    FoodContainer genotype(foods);
+    auto mutation = genotype.find(iter->first);
 
+    // + mutation
+    // - mutation
+
+    //if error < error0, add to container
+  }
 }
