81434279c10f21caec9c3653ef813bd430895245
Serafima Zykova <zykovas91@gmail.com>
2017-11-16 15:20:32 +0300

    copyable node

--- Nutrition/Node.h
+++ Nutrition/Node.h
@@ -18,30 +18,37 @@ public:
     , sub_()
   {}
 
-  Node(const T& body, const std::list<Node*>& sub)
+  Node(const T& body, const std::list<Node>& sub)
     : body_(body)
     , sub_(sub)
   {}
 
-  Node(const Node& other) = delete;
-  Node& operator =(const Node& other) = delete;
+  Node(const Node& other)
+    : body_(other.body_)
+    , sub_(other.sub_)
+  {}
+
+  Node operator =(const Node& other)
+  {
+    return Node(other);
+  }
 
   virtual ~Node() = default;
 
-  void setSub(const std::list<Node*>& sub) { sub_ = sub; }
+  void setSub(const std::list<Node>& sub) { sub_ = sub; }
 
   const T& getBody() const { return body_; }
-  const std::list<Node*>& getSub() const { return sub_; }
+  const std::list<Node>& getSub() const { return sub_; }
 
 
-  typename std::list<Node*>::iterator eraseSub(typename std::list<Node*>::const_iterator iter)
+  typename std::list<Node>::iterator eraseSub(typename std::list<Node>::const_iterator iter)
   {
     return sub_.erase(iter, sub_.end());
   }
 
 private:
   T body_;
-  std::list<Node*> sub_;
+  std::list<Node> sub_;
 };
 
 #endif // NODE_H
